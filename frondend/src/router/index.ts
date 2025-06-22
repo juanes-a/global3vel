cafff9f23655ad00184c84574e9b60374285fcd0
Juan <juannomesque12@gmail.com>
2025-06-22 00:32:55 -0500

    Fix redirects and clean build

--- frondend/src/router/index.ts
+++ frondend/src/router/index.ts
@@ -1,30 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../components/mapGlobe.vue' 
-import aboutUs from '../view/aboutUs.vue' // Importa tu componente de "Sobre nosotros"
-import gameStats from '../view/gameStats.vue' // Importa tu componente de estadísticas del juego
-
-
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView // Tu componente con el globo terráqueo
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: aboutUs
-  },
-  {
-    path: '/stats',
-    name: 'stats',
-    component: gameStats
-  }
-]
+import HomeView from '../components/mapGlobe.vue'
+import aboutUs from '../view/aboutUs.vue' // Cambié 'view' por 'views'
+import gameStats from '../view/gameStats.vue' // Cambié 'view' por 'views'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: aboutUs
+    },
+    {
+      path: '/stats',
+      name: 'stats',
+      component: gameStats
+    },
+    // Ruta de fallback para SPA
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/'
+    }
+  ]
 })
 
 export default router
